5122cf9f047967d2f0d8d60ed46cdbeb8e770e50
Sahil Nagpal <wowitssahil@gmail.com>
2018-03-20 00:00:39 +0530

    More functions

---
+++ R_Book-Programming/Functions_OddEvenandMore.R
@@ -0,0 +1,39 @@
+check.fun<-function(num)  {	
+  num = as.integer(readline(prompt="Enter a number: "))
+  if((num %% 2) == 0) {
+    print(paste(num,"is Even"))
+  } else {
+    print(paste(num,"is Odd"))
+  }
+}
+
+
+tricky<-function(num){
+  num=as.integer(readline(prompt = "Please enter a number  : "))
+  if((num%%3)==0){
+    print(paste("I am 3"))
+  }else if((num%%5)==0){
+    print(paste("I am 5"))
+  }else if((num%%7)==0){
+    print(paste("I am 7"))
+  }else if((num%%9)==0){
+    print(paste("I am 9"))
+  }else{
+    print("Fuck off")
+  }
+}
+
+tricky()
+
+
+
+check.fun()
+
+divisbleby3=function(num){
+  num=as.integer(readline(prompt = "Please enter a number to check : "))
+  if((num%%3) == 0){
+    print("Number is divisble by 3")
+  }else{
+    print("Number is not divisble by 3")
+  }
+}
